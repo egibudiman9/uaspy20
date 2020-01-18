ea8cb5c410409d6df84ccdaec1ff99b762524635
egibudiman9 <56252077+egibudiman9@users.noreply.github.com>
2020-01-18 18:49:10 +0700

    Add files via upload

---
+++ uaspy20-master/README.md
@@ -0,0 +1,3 @@
+# Project UAS Bahasa Pemrograman
+silakan tuliskan langkah-langkah disertai screenshotnya
+![uaspy20  C__Users_Daul_PycharmProjects_uaspy20  -  _main py - PyCharm 18_01_2020 18_15_28](https://user-images.githubusercontent.com/53391777/72662861-9915e400-3a1e-11ea-9633-d276e06d512a.png)
---
+++ uaspy20-master/main.py
@@ -0,0 +1,20 @@
+from view.view_book import*
+from view.input_book import*
+from core.baseapp import BaseApp
+from core.search_helper import SearchHelper
+
+class MainApp(BaseApp):
+    def __init__(self):
+        self.books = []
+
+    def list_book(self):
+        ViewBook.list(self)
+
+    def add_book(self):
+        InputBook.input(self)
+        SearchHelper.search_title(self)
+
+if __name__ == "__main__":
+    app = MainApp()
+    app.run()
+
